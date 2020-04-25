b59b1a493e08e843550d0833b1d7eab55741d560
Johnathan Bressette <bressetteprogramming@gmail.com>
2020-04-25 18:25:04 -0400

    Add user ids

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
 const discord = require('discord.js');
 const client = new discord.Client;
 const gayNames = ["Bryce", "Ian", "Raymond"];
+const gayUserIds = ["<@!326546102537158666>", "<@!354954586529726465>", "<@!300054377505226752>"];
 
 client.on('message', message => {
     //code used because my friends are not very creative and call each other gay every half second
     for(var i in gayNames) {
         if(message.content.toLowerCase().includes(gayNames[i].toLowerCase())) {
-            message.channel.send("Gay");
+            message.channel.send(gayUserIds[i] + " Gay");
         }
     }
 });
