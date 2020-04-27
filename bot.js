e27ecac834fca272dc3226678bce43ee98fae129
Johnathan Bressette <bressetteprogramming@gmail.com>
2020-04-27 12:15:43 -0400

    Add check for e! flag

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ const dbName = 'ela-bot'
 const MongoClient = require('mongodb').MongoClient
 const url = 'mongodb://127.0.0.1:27017'
 let db
+let userTag
 
 MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true}, (err, dbClient) => {
     if(err) return console.log(err)
@@ -16,8 +17,16 @@ MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true}, (er
 
 client.on('message', message => {
     //code used because my friends are not very creative and call each other gay every half second
-    
-    const content = message.content;
+    userTag = message.member.user.tag;
+    content = message.content;
+
+    if(content.trim().startsWith("e!", 0)) {
+        content = content.substr(2, content.length)
+        content = content.trim()
+        if(content.localeCompare("daily") === 0) {
+            message.channel.send("You claimed your daily reward of 5000$, You can claim it again in 24h")
+        }
+    }
 
     for(var i in gayNames) {
         if(content.toLowerCase().includes(gayNames[i].toLowerCase())) {
