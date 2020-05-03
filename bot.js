582c992703d2b4fdf0983f013a88e532d24b5988
Johnathan Bressette <bressetteprogramming@gmail.com>
2020-05-03 12:26:10 -0400

    Add kick command

--- bot.js
+++ bot.js
@@ -64,6 +64,23 @@ client.on('message', message => {
             case "leaderboard":
                 economy.leaderboard(message, client)
                 break
+            case "kick":
+                if(message.member.hasPermission("ADMINISTRATOR"))
+                {
+                    member = message.mentions.members.first()
+                    member.kick().then((member) =>
+                    {
+                        message.channel.send(":wave: " + member.displayName + " has been kicked from the server :sunglasses: ")
+                    }).catch(() =>
+                    {
+                        console.log("Error")
+                    })
+                }
+                else
+                {
+                    message.channel.send("You do not have the permission to do that :rage:")
+                }
+                break
             case "sekiro":
                 message.channel.send(sekiroReview)
                 break
