57d7254737b0ca02217af1fb29752f0feee727b6
Johnathan Bressette <bressetteprogramming@gmail.com>
2020-05-07 14:35:38 -0400

    Add basic youtube playing functionality

--- bot.js
+++ bot.js
@@ -5,6 +5,15 @@ const economy = require('./economy')
 const admin = require('./admin')
 const token = require('./token.json')
 const copyPastas = require('./copy-pastas.json')
+const music = require('./music.js')
+const queue = new Map()
+const ytdl = require('ytdl-core')
+const search = require('youtube-search')
+
+var opts = {
+    maxResults: 10,
+    key: 'AIzaSyDTgw5RCq55zJmbssgtxtXjOkv4bzYOYJ4'
+}
 
 mongoUtil.connectToServer(() =>
 {
@@ -22,15 +31,14 @@ client.on('ready', () =>
     .catch(console.error)
  })
 
-client.on('message', message => {
-    
+client.on('message', async message => 
+{
+    if(message.author.bot)
+        return
     userTag = message.member.user.tag
     userId = message.member.user.id
     content = message.content
 
-    //changes all characters to lower case to allow the commands to not be case sensitive
-    content = content.toLowerCase()
-    
     
     //check if content starts with the command prefix e!
     if(content.trim().startsWith("e!", 0)) 
@@ -39,11 +47,13 @@ client.on('message', message => {
         content = content.substr(2, content.length).trim()
 
         //create a command string to hold the command keyword
-        command = content
+        command = content.toLowerCase()
         if(command.includes(" ")) 
         {
             command = command.substr(0, content.indexOf(" "))
         }
+
+        const serverQueue = queue.get(message.guild.id)
 
         //switch statement to determine what command the user used
         switch(command) 
@@ -81,8 +91,112 @@ client.on('message', message => {
             case "sekiro":
                 message.channel.send(copyPastas.sekiro)
                 break
+            case "play":
+                searchKeywords = content.substr(4, content.length)
+                console.log("The search keywords are: " + searchKeywords)
+                search(searchKeywords, opts, function(err, results) 
+                {
+                    if(err) return console.log("This is an error\n" + err)
+
+                    console.log("The results from search are\n" + results[0].link)
+                    execute(message, results[0].link, serverQueue)
+
+                })
+                break
+            case "skip":
+                skip(message, serverQueue)
+            case "reset":
+                stop(message, serverQueue)
+                
         }
     }
 })
+
+async function execute(message, link, serverQueue) {
+    
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel)
+      return message.channel.send(
+        "You need to be in a voice channel to play music!"
+      );
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+      return message.channel.send(
+        "I need the permissions to join and speak in your voice channel!"
+      );
+    }
+  
+    const songInfo = await ytdl.getInfo(link);
+    const song = {
+      title: songInfo.title,
+      url: songInfo.video_url
+    };
+  
+    if (!serverQueue) {
+      const queueContruct = {
+        textChannel: message.channel,
+        voiceChannel: voiceChannel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true
+      };
+  
+      queue.set(message.guild.id, queueContruct);
+  
+      queueContruct.songs.push(song);
+  
+      try {
+        var connection = await voiceChannel.join();
+        queueContruct.connection = connection;
+        play(message.guild, queueContruct.songs[0]);
+      } catch (err) {
+        console.log(err);
+        queue.delete(message.guild.id);
+        return message.channel.send(err);
+      }
+    } else {
+      serverQueue.songs.push(song);
+      return message.channel.send(`${song.title} has been added to the queue!`);
+    }
+  }
+  
+  function skip(message, serverQueue) {
+    if (!message.member.voice.channel)
+      return message.channel.send(
+        "You have to be in a voice channel to stop the music!"
+      );
+    if (!serverQueue)
+      return message.channel.send("There is no song that I could skip!");
+    serverQueue.connection.dispatcher.end();
+  }
+  
+  function stop(message, serverQueue) {
+    if (!message.member.voice.channel)
+      return message.channel.send(
+        "You have to be in a voice channel to stop the music!"
+      );
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end();
+  }
+  
+  function play(guild, song) {
+    const serverQueue = queue.get(guild.id);
+    if (!song) {
+      serverQueue.voiceChannel.leave();
+      queue.delete(guild.id);
+      return;
+    }
+  
+    const dispatcher = serverQueue.connection
+      .play(ytdl(song.url))
+      .on("finish", () => {
+        serverQueue.songs.shift();
+        play(guild, serverQueue.songs[0]);
+      })
+      .on("error", error => console.error(error));
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    serverQueue.textChannel.send(`Started playing: **${song.title}**`);
+  }
 
 client.login(token.token)
