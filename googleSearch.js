2cdb57539a112517d0b0c627d38add2d3e5fbd99
Johnathan Bressette <bressetteprogramming@gmail.com>
2020-05-28 21:48:36 -0400

    Add ability to show 10 results

--- googleSearch.js
+++ googleSearch.js
@@ -4,16 +4,49 @@ const config = require('./config.json')
 const client = new imageSearch(config.csekey, config.googlekey)
 const options = {page:1}
 
+let isFullResults
+
+
 module.exports =
 {
     search : (message, content) =>
     {
         content = content.substr(11, content.length).trim()
-        console.log(content)
+        if(content.startsWith("-full"))
+        {
+            isFullResults = true
+            content = content.substr(5, content.length).trim()
+        }
+
+        else
+        {
+            isFullResults = false
+        }
+
         client.search(content, options)
         .then(images =>
         {
-            message.channel.send(images[0].url)
+            if(isFullResults)
+            {
+                results = ["",""]
+                for(i in images)
+                {
+                    if(i < 5)
+                        results[0] += (images[i].url + "\n")
+                    else if(i < 10)
+                        results[1] += (images[i].url + "\n")
+                }
+
+                message.channel.send(results[0])
+                message.channel.send(results[1])
+            }
+
+            else
+            {
+                message.channel.send(images[0].url)
+            }
+
+            
         }).catch(error => console.log(error))
     }
 }
