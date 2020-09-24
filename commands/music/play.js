e3489fefa63c868b0d9ba5e1557749e2bbbf90b4
Johnathan Bressette <bressetteprogramming@gmail.com>
2020-09-23 21:31:57 -0400

    Clean up the code for the play command

--- commands/music/play.js
+++ commands/music/play.js
@@ -10,19 +10,24 @@ module.exports =
     name: "play",
     description: "Searches and plays a song from youtube",
     aliases: ['p'],
+
+
     async execute(message, args)
     {
         serverQueue = message.client.queue.get(message.guild.id)
-
         content = message.content
 
-      if((content === "play" || content === "p") && serverQueue.connection.dispatcher.paused)
-      {
-          return serverQueue.connection.dispatcher.resume()
-      }
+        if((content === "play" || content === "p") && serverQueue.connection.dispatcher.paused)
+        {
+            return serverQueue.connection.dispatcher.resume()
+        }
 
-      content = content.substr(content.indexOf(args[0]), content.length)
-        const voiceChannel = message.member.voice.channel;
+        searchKeywords = content.substr(content.indexOf(args[0]), content.length)
+
+
+        const voiceChannel = message.member.voice.channel; //initialize a voiceChannel object to join the voice channel
+
+        //check that the user is in a voice channel and the bot has the permissions to connect and speak
         if (!voiceChannel)
           return message.channel.send(
             "You need to be in a voice channel to play music!"
@@ -34,64 +39,18 @@ module.exports =
           );
         }
 
-        searchKeywords = content
-        link = ""
-        if(ytdl.validateURL(searchKeywords))
-            link = searchKeywords
-        else
-        {
-            options = {limit: 10}
-            values = await search(searchKeywords, options)
-            
-            if(values.items.length != 0)
-            {
-                i = 0
-                while(values.items[i].type === "playlist" || values.items[i].type === "channel" || values.items[i].link === undefined || values.items[i].type === "movie")
-                {
-                    i++
-                }
-
-                if(values.items[i].link === undefined)
-                    link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-                else
-                    link = values.items[i].link
-            }
-
-            else
-            {
-                message.channel.send("That video does not exist!")
-                link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-            }
-        }
-
-        // console.log(i)
-        console.log(link)
-        console.log(values)
-        let songInfo
-          while(true)
-          {
-            try {
-              songInfo = await ytdl.getInfo(values.items[i].link)
-              break
-            } catch(err) {
-              console.log(err)
-              if(i > 9)
-                songInfo = await ytdl.getInfo("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-              else
-                i++
-            }
-          }
-            
-       
+        //gets the song info(title and video url) from the first applicable result
+        songInfo = await module.exports.getSongInfo(message, searchKeywords)
         
-        
-
+        //creates an object to store the song details
         const song = {
           title: songInfo.title,
           url: songInfo.video_url
         };
       
-        if (!serverQueue) {
+        //if there are no songs in the queue create a new queue object
+        if (!serverQueue) 
+        {
           const queueContruct = {
             textChannel: message.channel,
             voiceChannel: voiceChannel,
@@ -101,63 +60,106 @@ module.exports =
             playing: true
           };
       
+          //set the queue for the guild and push a song into the queue
           message.client.queue.set(message.guild.id, queueContruct);
-      
           queueContruct.songs.push(song);
       
+          //try to join the voiceChannel and play the song if an exception occurs the queue is deleted
           try {
-            var connection = await voiceChannel.join();
-            queueContruct.connection = connection;
-            module.exports.play(message, message.guild, queueContruct.songs[0]);
+              var connection = await voiceChannel.join();
+              queueContruct.connection = connection;
+              module.exports.play(message, message.guild, queueContruct.songs[0]);
           } catch (err) {
-            console.log(err);
-            message.client.queue.delete(message.guild.id);
-            return message.channel.send(err);
+              console.error(err);
+              message.client.queue.delete(message.guild.id);
+              return message.channel.send(err);
           }
-        } else 
+        }
+        else 
         {
-            console.log(message)
-            if(message.content.startsWith("-restart"))
-                serverQueue.songs.unshift(song)
-            else
-            {
-                serverQueue.songs.push(song);
-                message.channel.send(`${song.title} has been added to the queue!`);
-                printQueue.execute(message, args)
-            }
-          
+            serverQueue.songs.push(song);
+            message.channel.send(`${song.title} has been added to the queue!`);
+            printQueue.execute(message, args)
         }
     },
 
     async play(message, guild, song)
     {
         const serverQueue = message.client.queue.get(guild.id);
+        //if there is no song delete the queue and leave the voice channel
         if (!song) {
-          serverQueue.voiceChannel.leave();
-          message.client.queue.delete(guild.id);
-          return;
+            serverQueue.voiceChannel.leave();
+            message.client.queue.delete(guild.id);
+            return;
         }
 
+        //create the dispatcher to play the audio in the voice channel
         const dispatcher = serverQueue.connection
-          .play(ytdl(song.url))
-          .on("finish", async() => {
+            .play(ytdl(song.url))
+            .on("finish", async() => {
 
-            loop = await getLoop.execute(guild)
-            if(loop === undefined)
-                loop = false
+                //check if the current song should loop
+                loop = await getLoop.execute(guild)
+                if(loop === undefined)
+                    loop = false
 
-            if(!loop)
-              serverQueue.songs.shift();
+                //if loop isn't enabled shift the queue to get the next song
+                if(!loop)
+                  serverQueue.songs.shift();
 
-            module.exports.play(message, guild, serverQueue.songs[0]);
+                //call play recursively to play the next song
+                module.exports.play(message, guild, serverQueue.songs[0]);
           })
-          .on("error", error => console.log("In Error"));
-        dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+          .on("error", error => console.error(error));
 
-        loop = await getLoop.execute(guild)
-        if(loop === undefined)
-            loop = false
-        if(!loop)
-          serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+        dispatcher.setVolumeLogarithmic(serverQueue.volume / 5); //sets the volume to 1/5 of the max on the first execution
+        serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+    },
+
+    //method that is used to get the song title and url 
+    async getSongInfo(message, searchKeywords) {
+
+        //if the message contains a valid url return the associated info object
+        if(ytdl.validateURL(searchKeywords))
+            return await ytdl.getInfo(searchKeywords)
+        
+        //if the user did not provide a valid url search for a video with the given string
+        else
+        {
+            options = {limit: 10} //limit the search results to 10 videos
+            values = await search(searchKeywords, options) //store the search results in values
+            
+            //if the search result is not empty remove results that cannot be played and return the song info for the link that can be played
+            if(values.items.length != 0)
+            {
+                i = 0
+                while(values.items[i].type === "playlist" || values.items[i].type === "channel" || values.items[i].link === undefined || values.items[i].type === "movie")
+                {
+                    i++
+                }
+
+                //loop that iterates over the search results until a video that can be played is found.
+                while(true)
+                {
+                    let songInfo
+                    try {
+                        songInfo = await ytdl.getInfo(values.items[i].link)
+                        return songInfo
+                    } catch(err) {
+                        console.error(err)
+                        console.log(i)
+                        if(i > 9)
+                            return await ytdl.getInfo("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+                        else
+                            i++
+                    }
+                }
+            }
+            else
+            {
+                message.channel.send("That video does not exist!")
+                return await ytdl.getInfo("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+            }
+        }
     }
 }
