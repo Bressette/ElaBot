dd91e6ac1902efde315c20a0eda3dbea131faee0
Johnathan Bressette <bressetteprogramming@gmail.com>
2020-10-12 18:51:20 -0400

    Rename stop.js to reset.js

---
+++ commands/music/reset.js
@@ -0,0 +1,21 @@
+module.exports = 
+{
+    name: "reset",
+    description: "Forces the bot to leave the voice channel and remove the music queue",
+    aliases: ['r'],
+    execute(message, args)
+    {
+        if (!message.member.voice.channel)
+        return message.channel.send(
+          "You have to be in a voice channel to stop the music!"
+        );
+
+        serverQueue = message.client.queue.get(message.guild.id)
+
+        if(serverQueue.connection.dispatcher.paused)
+            serverQueue.connection.dispatcher.resume()
+        
+        serverQueue.songs = [];
+        serverQueue.connection.dispatcher.end();
+    }
+}
